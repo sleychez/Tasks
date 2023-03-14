0f3e7c87c7438715ec5fdbcf6cada5b8b5283052
sleychez <v0401podtyagin@mail.ru>
2023-03-14 22:38:00 +0300

    Task4 added block traffic light

---
+++ src/pages/Task4/Task4.tsx
@@ -0,0 +1,48 @@
+import React, {FC, useRef, useState} from 'react';
+
+import * as ST from './styled'
+
+
+
+const Task4: FC = () => {
+
+   const [text, setText] = useState('')
+
+    const [color, setColor] = useState('red')
+
+    const value = useRef<string>()
+
+    const changeColor = () => {
+       if (color === 'yellow') {
+           setColor(value.current === 'red' ? 'green' : 'red' )
+       }
+       if (color === 'green') {
+           value.current = 'green'
+           setColor('yellow')
+           }
+       if (color === 'red') {
+           value.current = 'red'
+           setColor('yellow')
+       }
+    }
+
+    const hover = () => {
+       setText('hover')
+    }
+
+    const leave = () => {
+       setText('out')
+    }
+
+    const down = () => {
+        setText('click')
+    }
+
+
+    return (
+        <ST.Block onPointerEnter={hover} onClick={changeColor} color={color} onPointerLeave={leave} onMouseDown={down} onMouseUp={hover}>{text}
+        </ST.Block>
+    );
+};
+
+export default Task4;
--- src/styled.ts
+++ src/styled.ts
@@ -1,25 +1,11 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+
+
+
 
 
 export const App = styled.div`
   display: flex;
   flex-direction: column;
   
-`
-export const Header = styled.header`
-  display: flex;
-  gap: 20px;
-  margin-left: 30px
-`
-
-export const StyledLink = styled(Link)`
-  color: black;
-  text-decoration: none;
-  display: flex;
-  font-weight: bold;
-
-  &:hover {
-    color: red;
-  }
 `
